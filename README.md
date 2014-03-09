ac4869ba975068080a4e978f98cfed9a870e69f0
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-03-09 19:32:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+rework-plugin-colors
+====================
+
+colors() plugin for rework, formerly included in core
---
+++ test/rework-colors.js
@@ -0,0 +1,26 @@
+
+var rework = require('rework')
+  , inline = require('../')
+  , fs = require('fs')
+  , assert = require('assert')
+  , read = fs.readFileSync;
+
+function fixture(name) {
+  return read('test/fixtures/' + name + '.css', 'utf8').trim();
+}
+
+describe('.colors()', function(){
+  it('should support rgba(color, a)', function(){
+    rework(fixture('colors'))
+      .use(rework.colors())
+      .toString()
+      .should.equal(fixture('colors.out'));
+  })
+
+  it('should support hsb(hue, saturation, value)', function(){
+    rework(fixture('colors.hsb'))
+      .use(rework.colors())
+      .toString()
+      .should.equal(fixture('colors.hsb.out'));
+  })
+})
